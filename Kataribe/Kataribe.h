389c5dd030eb13f4d94254911f39ae02f3b2e353
hiragram <y@yura.me>
2018-11-25 12:48:48 +0900

    Add Kataribe library module

---
+++ Kataribe/Kataribe.h
@@ -0,0 +1,19 @@
+//
+//  Kataribe.h
+//  Kataribe
+//
+//  Created by Yuya Hirayama on 2018/11/25.
+//  Copyright © 2018年 Yuya Hirayama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kataribe.
+FOUNDATION_EXPORT double KataribeVersionNumber;
+
+//! Project version string for Kataribe.
+FOUNDATION_EXPORT const unsigned char KataribeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kataribe/PublicHeader.h>
+
+
