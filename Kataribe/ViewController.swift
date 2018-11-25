130c7da3234b33b6241e5fd532a9b4f9f5fe7cdb
hiragram <y@yura.me>
2018-11-25 12:40:22 +0900

    Add Xcode project

---
+++ Kataribe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kataribe
+//
+//  Created by Yuya Hirayama on 2018/11/25.
+//  Copyright © 2018年 Yuya Hirayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
